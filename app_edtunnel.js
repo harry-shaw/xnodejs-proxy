3f45daeff4718f53bec229d58eb2c2183576b347
harry-shaw <harry-shaw@qq.com>
2023-07-31 08:02:32 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'b6cdd1a3-7b77-4d1f-9b63-3252cd24021f';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
